09beba5dd8410884603f63e4b1f028cbee4b1ec0
white-hun <121800818+white-hun@users.noreply.github.com>
2023-02-23 22:19:59 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.box}>
+        <Text>1번</Text>
+      </View>
+      <View>
+        <Text style={styles.box}>2번</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +19,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  box: {
+    backgroundColor: "blue",
+    marginBottom: 8,
+    width: 100,
+    height: 100,
   },
 });
